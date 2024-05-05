4d0304e6c6a39a1227ab0ad908a2cdc12ec3e78f
hamm22 <123744435+hamm22@users.noreply.github.com>
2024-05-06 01:13:56 +0900

    로그아웃(#3) 완료

--- src/main/java/com/ham/habit/user/UserController.java
+++ src/main/java/com/ham/habit/user/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @Controller
 public class UserController {
@@ -16,5 +19,17 @@ public class UserController {
 	@GetMapping("/login-view")
 	public String inputLogin() {
 		return "user/login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request){
+		
+		HttpSession session = request.getSession();
+		
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		
+		// 로그인 페이지로
+		return "redirect:/user/login-view";
 	}
 }
